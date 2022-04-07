33f2cfe97cf927ccef13b7bc7ab1d3defc83e828
NatalieClark2021 <NatalieClark2021@gmail.com>
2022-04-06 21:31:19 -0500

    week 13 exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basic-ngFor-exercise';
+  foods : string[] = ["beef", "pork", "chicken"];
 }
